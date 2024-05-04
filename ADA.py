d923de758b3cafceda1b8f6c7a1c95b8e63fc4b8
Nathan <140237302+NathanVanR@users.noreply.github.com>
2024-05-04 18:02:06 +0200

    ADA audio update 1
    The first iteration of ADA's audio capabilities. Ideally, I'd like to separate the playing of the audio with the generation of the response, but this is sufficient for now.

--- ADA.py
+++ ADA.py
@@ -2,6 +2,11 @@ import customtkinter as ctk
 import cohere
 import uuid
 import os
+from transformers import pipeline
+from datasets import load_dataset
+import soundfile as sf
+import torch
+from playsound import playsound
 
 # conversation_id is for conversation history to help with context
 conversation_id = str(uuid.uuid4())
@@ -11,7 +16,19 @@ api_key = os.environ.get('COHERE_API_KEY')
 co = cohere.Client(api_key=api_key)
 
 # preamble is to help with better responses
-preamble = "You are my AI assistant, ADA. You are named after the famous computer scientist, Ada Lovelace."
+preamble = "You are my AI assistant, ADA. You are named after the famous computer scientist, Ada Lovelace. Please keep all responses to a maximum of 600 characters."
+
+def play_audio(text):
+    synthesiser = pipeline("text-to-speech", "microsoft/speecht5_tts")
+    embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+    speaker_embedding = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+    # You can replace this embedding with your own as well.
+
+    speech = synthesiser(text, forward_params={"speaker_embeddings": speaker_embedding})
+
+    sf.write("speech.wav", speech["audio"], samplerate=speech["sampling_rate"])
+    audio = "speech.wav"
+    playsound(audio)
 
 # this is the chat function
 def chat(co, preamble, conversation_id, user_input):    # user input is a CTKEntry field
@@ -25,10 +42,14 @@ def chat(co, preamble, conversation_id, user_input):    # user input is a CTKEnt
     answer_text.insert(ctk.END,"ADA: {}\n\n".format(response.text))
     answer_text.configure(state="disabled") # reset TextBox state to "disabled" to disallow changes
     user_input.delete(0,'end')
+    play_audio(response.text)
+
 
 # enables the 'enter' key to trigger chat function
 def enter(event):
     chat(co, preamble, conversation_id, user_input)
+
+
     
 ctk.set_appearance_mode("dark")
 root = ctk.CTk()
@@ -51,6 +72,7 @@ submit_button.pack(pady=12,padx=5)
 answer_text = ctk.CTkTextbox(master=frame, width=500,corner_radius=10,wrap='word')
 answer_text.configure(state="disabled") # set state to disabled to disallow users to edit output
 answer_text.pack(pady=6,padx=5)
+
 
 user_input.focus()
 
